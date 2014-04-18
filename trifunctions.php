5bbd80bd9c6c12eac3e8dc4f1f8221cfce72cd4d
Maria Elena Marinelli <mm3688@drexel.edu>
2014-04-17 22:09:29 -0400

    Widgetized Theme
    Added widget capability to the right sidebar.

--- trifunctions.php
+++ trifunctions.php
@@ -145,6 +145,22 @@ add_action( 'admin_menu', 'change_post_menu_label' );
 // 	return $avatar_defaults;
 // }
 
+
+//Widgetizing
+function arphabet_widgets_init() {
+
+	register_sidebar( array(
+		'name' => 'Home right sidebar',
+		'id' => 'home_right_1',
+		'before_widget' => '<div>',
+		'after_widget' => '</div>',
+		'before_title' => '<h1 class="rounded">',
+		'after_title' => '</h1>',
+	) );
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
+
+
 //Register Multimedia Post Type
 
 function post_type_init() {
