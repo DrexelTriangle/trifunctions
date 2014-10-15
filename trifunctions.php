08ff719d9da7553bffea6f37e9278a395df8d709
Noel Forté <ntforte@gmail.com>
2014-10-15 13:45:36 -0400

    New GA Script

--- trifunctions.php
+++ trifunctions.php
@@ -82,18 +82,19 @@ function wc_favicons_admin() {
 }
 add_action ('admin_head', 'wc_favicons_admin');
 
-function wc_ga_code() {
-	echo "<script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+function wc_ga_code() { ?>
 
-	ga('create', 'UA-162588-4', 'auto');
-	ga('require', 'displayfeatures');
-	ga('send', 'pageview');
-	</script>";
-}
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-162588-7', 'auto');
+	  ga('send', 'pageview');
+	</script>
+
+<?php }
 add_action( 'wp_footer', 'wc_ga_code');
 
 //Excerpt Settings
@@ -326,9 +327,8 @@ class Single_Post_Template {
 
 	}
 
-	function metabox( $post ) {
+	function metabox( $post ) { ?>
 
-		?>
 		<input type="hidden" name="pt_noncename" id="pt_noncename" value="<?php echo wp_create_nonce( plugin_basename( __FILE__ ) ); ?>" />
 
 		<label class="hidden" for="post_template"><?php  _e( 'Post Template', 'genesis' ); ?></label><br />
@@ -337,9 +337,8 @@ class Single_Post_Template {
 			<?php $this->post_templates_dropdown(); ?>
 		</select><br /><br />
 		<p><?php _e( 'Some themes have custom templates you can use for single posts that might have additional features or custom layouts. If so, you will see them above.', 'genesis' ); ?></p>
-		<?php
-
-	}
+		
+	<?php }
 
 	function metabox_save( $post_id, $post ) {
 
