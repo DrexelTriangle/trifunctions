e1879e6a1d83f039ac13fd574a188abd7392a7d1
Brandon Kolle <bk584@drexel.edu>
2019-04-13 21:23:45 -0400

    Added new google analytics code

--- trifunctions.php
+++ trifunctions.php
@@ -84,17 +84,17 @@ add_action ('admin_head', 'wc_favicons_admin');
 
 function wc_ga_code() { ?>
 
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-162588-4"></script>
 	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
 
-	  ga('create', 'UA-162588-7', 'auto');
-	  ga('send', 'pageview');
+	gtag('config', 'UA-162588-4');
 	</script>
 
 <?php }
+
 add_action( 'wp_head', 'wc_ga_code');
 
 //Add wrappers to media elements.
